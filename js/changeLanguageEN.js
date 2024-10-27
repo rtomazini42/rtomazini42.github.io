98aff47b5bb5b7ad40f932df56e13f2d92f0f871
renan tomazini <renantomazini@gmail.com>
2024-10-26 23:12:32 -0300

    Inglês adicionado

---
+++ js/changeLanguageEN.js
@@ -0,0 +1,3 @@
+function loadNewPage() {
+    window.location.href = "index_en.html"; // Substitua "novo_arquivo.html" pelo caminho do HTML que deseja carregar
+}
---
+++ js/changeLanguagePT.js
@@ -0,0 +1,3 @@
+function loadNewPage() {
+    window.location.href = "index.html"; // Substitua "novo_arquivo.html" pelo caminho do HTML que deseja carregar
+}
